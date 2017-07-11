73d21aa370a8516417848dba9215be49cc588c34
Alex Burrows <alex@alexburrows.net>
2017-07-11 21:10:25 +0100

    added digidrop base theme

---
+++ web/themes/custom/digidrop/js/custom.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * Custom js.
+ */
+
+(function ($) {
+
+  'use strict';
+
+  Drupal.behaviors.digidropglobal = {
+    attach: function () {
+
+      $('.nav-btn.closed').click(function(){
+        //$(this).hasClass('closed');
+        $('.nav-overlay').fadeIn(800);
+      });
+
+    }
+  }
+
+})(jQuery);
